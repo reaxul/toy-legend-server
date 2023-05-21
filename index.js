0926a45a20ee1a531da8f1f78eaad87f3a90ad5f
DevReaxul <hreazul11@gmail.com>
2023-05-21 14:10:52 +0600

    single data loaded

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -40,6 +40,13 @@ async function run() {
                 query = { sellerEmail: req.query.sellerEmail }
             }
             const result = await toyCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.get('/details/:id', async (req, res) => { 
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await toyCollection.findOne(query);
             res.send(result);
         })
 

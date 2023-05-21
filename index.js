8e3c6d899ae45050581e3a76d905eca88c591ee4
DevReaxul <hreazul11@gmail.com>
2023-05-21 10:06:41 +0600

    data inserted to mongodb

--- index.js
+++ index.js
@@ -25,7 +25,17 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+      await client.connect();
+      
+      const toyCollection = client.db('toyLegend').collection('toyCollection');
+
+      app.post('/toy-collection', async (req, res) => { 
+          const toys = req.body;
+          console.log(toys);
+          const result = await toyCollection.insertOne(toys);
+          res.send(result);
+      })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
